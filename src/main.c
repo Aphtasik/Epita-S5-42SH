32fbd4491ae7dad49c826d39bd053936fc5d3af5
Alexandre Gautier <alexandre.gautier@epita.fr>
2021-11-26 10:19:05 +0100

    doc(main.c): add doc for token for lexer tests

--- src/main.c
+++ src/main.c
@@ -2,9 +2,22 @@
 
 #include <stdio.h>
 
+    /** 0 TOKEN_IF, */
+    /** 1 TOKEN_THEN, */
+    /** 2 TOKEN_ELIF, */
+    /** 3 TOKEN_ELSE, */
+    /** 4 TOKEN_FI, */
+    /** 5 TOKEN_SEMICOL, */
+    /** 6 TOKEN_BACKN, */
+    /** 7 TOKEN_SQUOTE, */
+    /** 8 TOKEN_WORD, */
+    /** 9 TOKEN_EOF, */
+    /** 10 TOKEN_ERROR, */
+    /** 11 TOKEN_UNKNOWN */
+
 int main(void)
 {
-    const char *test = "if tamere; then pute";
+    const char *test = "if tamere; then\n pute;";
     struct lexer *l = lexer_new(test);
     struct token *t;
     enum token_type type = TOKEN_IF;
