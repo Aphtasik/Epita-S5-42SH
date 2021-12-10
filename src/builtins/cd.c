743b91cb8cd592562b04cb6e7d93b1dbf5d918c9
lea.cruciani <lea.cruciani@epita.fr>
2021-12-10 18:31:57 +0100

    echo

---
+++ src/builtins/cd.c
@@ -0,0 +1,17 @@
+#include <err.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int cd(char **args, size_t len)
+{
+    char *home = getenv("HOME");
+    if (home)
+    {
+
+    }
+    else
+    {
+
+    }
+    return 0;
+}
